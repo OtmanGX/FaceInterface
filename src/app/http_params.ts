889ea296bca2202401b8b4bab7a64ab042416814
otmangx <otmangx@gmail.com>
2020-06-03 17:44:07 +0100

    fix bugs

--- src/app/http_params.ts
+++ src/app/http_params.ts
@@ -1,14 +1,25 @@
 
-export class HttpParams extends Map {
+export class HttpParams {
     public filterParams: Array<any>;
+    map: Map<any, any>;
     params: Array<any>;
-    // constructor() {
-    //     super();
-    //     this.filterParams = new Array();
-    // }
+    constructor() {
+        this.filterParams = new Array();
+        this.map = new Map();
+    }
     getParams() {
-        // return this.toObject(Array.from(this), this.filterParams);
-        return this.toObject(Array.from(this));
+        return this.toObject(Array.from(this.map), this.filterParams);
+        // return this.toObject(Array.from(this));
+    }
+
+    set(key , value) {
+        return this.map.set(key, value);
+    }
+    get(key) {
+        return this.map.get(key);
+    }
+    delete(key) {
+        return this.map.delete(key);
     }
     toObject(...arrays) {
         const res = {};
